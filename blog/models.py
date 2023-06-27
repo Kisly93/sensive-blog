54188b9072ba74d555667c45ded349e28de5ade0
Kisly93 <spsvk.kis.sov@gmail.com>
2023-06-27 17:20:36 +0200

    убрал лишнее

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,7 @@ class PostQuerySet(models.QuerySet):
 
     def popular(self):
         posts_popular = self.annotate(
-            likes_count=Count('likes'),
-            comments_count=Count('comments')
+        likes_count=Count('likes'),
         ).order_by('-likes_count')
         return posts_popular
 
@@ -20,7 +19,7 @@ class PostQuerySet(models.QuerySet):
     def fetch_with_comments_count(self):
         most_popular_posts_ids = [post.id for post in self]
         posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
-            comments_count=Count('comments'))
+        comments_count=Count('comments'))
         ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
         count_for_id = dict(ids_and_comments)
         for post in self:
